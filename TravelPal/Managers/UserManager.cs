fd0ba6ff9290488946a353d6d586e57b2784d5dc
JosefMontanari <josef.montanari94@gmail.com>
2023-10-24 14:52:54 +0200

    Update UserManager.cs
    Added all methods

--- TravelPal/Managers/UserManager.cs
+++ TravelPal/Managers/UserManager.cs
@@ -1,12 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Windows;
+using TravelPal.Classes;
 
 namespace TravelPal.Managers
 {
-    internal class UserManager
+    public class UserManager
     {
+        public static List<iUser> Users = new()
+        {
+            new User ("User", "password"),
+            new Admin{Username = "Admin", Password = "password"}
+        };
+
+        public iUser SignedInUser { get; set; }
+
+
+        public bool AddUser(iUser user)
+        {
+            if (ValidateUsername(user.Username))
+            {
+                if (ValidatePassword(user.Password))
+                {
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+
+        private bool ValidateUsername(string username)
+        {
+            bool isValid = true;
+            if (!string.IsNullOrWhiteSpace(username) && username.Length > 3)
+            {
+
+                foreach (iUser user in Users)
+                {
+                    if (user.Username == username)
+                    {
+                        MessageBox.Show("Sorry, that username is already taken!");
+                        isValid = false;
+                        return isValid;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Username needs to be at least three characters!");
+                isValid = false;
+            }
+
+            return isValid;
+
+        }
+
+        private bool ValidatePassword(string password)
+        {
+            bool isValid = true;
+            if (string.IsNullOrWhiteSpace(password) && password.Length < 3)
+            {
+                MessageBox.Show("Password needs to be at least three characters!");
+                isValid = false;
+            }
+            return isValid;
+
+        }
+
+        public void UpdateUsername(iUser user, string newUsername)
+        {
+            if (ValidateUsername(newUsername))
+            {
+                user.Username = newUsername;
+            }
+
+        }
+
+        public void RemoveUser(iUser user)
+        {
+            MessageBox.Show($"{user.Username} successfully removed!");
+            Users.Remove(user);
+        }
+
+        public bool SignInUser(string username, string password)
+        {
+            bool isValid = false;
+            foreach (iUser user in Users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    isValid = true;
+                    SignedInUser = user;
+                }
+
+            }
+
+            return isValid;
+        }
     }
 }
