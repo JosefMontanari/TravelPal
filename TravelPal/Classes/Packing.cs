e0da49e671e0023154c23470712e5209d288e048
JosefMontanari <josef.montanari94@gmail.com>
2023-10-25 11:28:51 +0200

    Create Packing.cs

---
+++ TravelPal/Classes/Packing.cs
@@ -0,0 +1,54 @@
+﻿namespace TravelPal.Classes
+{
+    public interface iPackingListItem
+    {
+        public string Name { get; set; }
+
+        public string GetInfo()
+        {
+            return Name;
+        }
+    }
+
+    public class TravelDocument : iPackingListItem
+    {
+        public string Name { get; set; }
+        public bool Required { get; set; }
+
+        public TravelDocument(string name, bool required)
+        {
+            Name = name;
+            Required = required;
+        }
+
+        public string GetInfo()
+        {
+            if (Required)
+            {
+                return Name + " Required!";
+            }
+            else
+            {
+                return Name + " Optional!";
+            }
+        }
+
+    }
+
+    public class OtherItem : iPackingListItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public OtherItem(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public string GetInfo()
+        {
+            return Name + " " + Quantity;
+        }
+    }
+}
